ade5ecfa51672d0f95338357330fd21a61790ca4
Rohit Shetake <99246721+codesMine@users.noreply.github.com>
2023-06-02 20:31:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import smtplib as s
 ob = s.SMTP('smtp.gmail.com', 587) # gmail  port number 587 , SMTP function used to  connection method
 ob.ehlo() 
 ob.starttls()
-ob.login('shetakerohit2@gmail.com', 'zikswpkcpyplfyca') #login requires aunthentication , use sending email account
+ob.login('email', 'type youe email password') #login requires aunthentication , use sending email account
 subject  = input("Enter The Subject Here :  ")  
 body = input("Enter Message Here :  ")
 msg = "subject:{}\n\n{}".format(subject, body) 
@@ -13,7 +13,7 @@ n = int(input("Enter the number of emails you sending :- "))
 for i in range(0, n):
     ele = input(f" [{i}] =>  ")
     listAdd.append(ele)
-ob.sendmail('shetakerohit2@gmail.com', listAdd, msg)   
+ob.sendmail('enter login email', listAdd, msg)   
 print("\n Sending.....")
 print("\nMail Send Successfully!")
 ob.quit()
