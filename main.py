8c3fe14b97abe3443a9c1287bd21397b6f09c5f2
Rohit Shetake <99246721+codesMine@users.noreply.github.com>
2023-06-02 20:29:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import smtplib as s
 ob = s.SMTP('smtp.gmail.com', 587) # gmail  port number 587 , SMTP function used to  connection method
 ob.ehlo() 
 ob.starttls()
-ob.login('shetakerohit2@gmail.com', 'zikswpkcpyplfyca') #login requires aunnthentication , use sending email account
+ob.login('shetakerohit2@gmail.com', 'zikswpkcpyplfyca') #login requires aunthentication , use sending email account
 subject  = input("Enter The Subject Here :  ")  
 body = input("Enter Message Here :  ")
 msg = "subject:{}\n\n{}".format(subject, body) 
-listAdd = ['shetakeyash@gmail.com']                     # adding reciver emails we can ad multiple email in list
+                     # adding reciver emails we can ad multiple email in list
+listAdd = []
+n = int(input("Enter the number of emails you sending :- "))
+for i in range(0, n):
+    ele = input(f" [{i}] =>  ")
+    listAdd.append(ele)
 ob.sendmail('shetakerohit2@gmail.com', listAdd, msg)   
-print(" Mail Send Successfully!")
+print("\n Sending.....")
+print("\nMail Send Successfully!")
 ob.quit()
